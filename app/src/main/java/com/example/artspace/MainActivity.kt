df385552fe79ef9c220becd046f093557d2857e9
Karan Kumar <karankumark19k@gmail.com>
2023-04-07 17:26:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun MainScreen(){
+
+}
